682f42de9b93589c1657f365c6bb42bf1625d36d
Lucas <luquillasojeda@gmail.com>
2023-10-17 11:34:55 -0300

    server changes

--- server/index.js
+++ server/index.js
@@ -17,15 +17,13 @@ const dbConfig = {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hola desde el servidor!" });
-});
-
 app.post("/", (req, res) => {
   const { dni, localidad, fecha, nombreAnimal, tipoAnimal } = req.body;
 });
 
 /*res.json({ message: "Datos cargados exitosamente" });*/
+
+app.post("/");
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
